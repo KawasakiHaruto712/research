7700a5e0e2fede8ac688cc38b91023964f603bb1
KawasakiHaruto712 <wakayama.kawasaki@gmail.com>
2024-07-21 18:53:04 +0900

    BertCrossValidation.pyのモデルを適切に保存できるように修正

--- processing_file/BERT/BertCrossValidation.py
+++ processing_file/BERT/BertCrossValidation.py
@@ -62,6 +62,7 @@ def train_and_evaluate(train_dataset, val_dataset, test_dataset, fold):
         logging_dir=f'./logs_fold_{fold}',  # ログ出力先
         logging_steps=10,
         evaluation_strategy="epoch"
+        save_strategy="epoch"  # この行を追加
     )
     # トレーナーの設定
     trainer = Trainer(
